b199dca30840cd379cab3e593b975ef37042bd6e
Molly <mili_lasschar@hotmail.com>
2024-11-27 20:42:40 -0300

    edit footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-between">
-        <span>COMPLETAR</span>
+        <span>COMPLETAR.</span>
         <p className="text-slate-600"></p>
       </div>
     </footer>
